27ef4076335c79423006e8685ba51552a04d01e2
Lucas Facci <50666911+lucasfacci@users.noreply.github.com>
2025-02-27 17:14:04 +0000

    feat: scripts to deploy the application

---
+++ tofu/aws/scripts/user-data.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Updating and upgrading packages
+yum update -y
+yum upgrade -y
+
+# Installing dependencies
+yum install git nginx python3-pip -y
+
+# Cloning repository
+git clone https://github.com/lucasfacci/aws-django-sms-api.git
+
+# Going to the root of the project
+cd aws-django-sms-api/django-sms-api/
+
+# Installing Python requirements
+pip install -r requirements.txt
+
+# Creating database and static folder
+python3 manage.py migrate
+python3 manage.py collectstatic
+
+# Creating Gunicorn service file
+cp ../tofu/aws/scripts/gunicorn.service /etc/systemd/system
+sed -i "s|VALUE1|$(openssl rand -base64 64 | tr -d '\n' | tr -d '/')|" /etc/systemd/system/gunicorn.service
+sed -i "s/VALUE2/$(curl -s http://checkip.amazonaws.com)/" /etc/systemd/system/gunicorn.service
+
+# Starting Gunicorn service
+systemctl start gunicorn
+systemctl enable gunicorn
+
+# Creating Nginx config file
+cp ../tofu/aws/scripts/nginx.conf /etc/nginx/nginx.conf
+sed -i "s/CHANGE_HERE/$(curl -s http://checkip.amazonaws.com)/" /etc/nginx/nginx.conf
+
+# Adding nginx user to the ec2-user group
+usermod -a -G ec2-user nginx
+
+# Giving execute permission in the home directory to the user group
+chmod 710 /home/ec2-user
+
+# Starting Nginx service
+systemctl start nginx
+systemctl enable nginx
