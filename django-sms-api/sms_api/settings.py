5377e041d183537607681f15745157e1fe4ffd71
lucasfacci <lucasfacci@protonmail.com>
2025-03-02 23:02:17 +0000

    feat: adding logging

--- django-sms-api/sms_api/settings.py
+++ django-sms-api/sms_api/settings.py
@@ -71,6 +71,24 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'sms_api.wsgi.application'
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': 'django.log',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
